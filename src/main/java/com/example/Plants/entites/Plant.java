3775b981ce9ed000cec96584393d7e12299d661c
llucasBorba <lucasborba6075@gmail.com>
2024-02-10 17:53:51 -0300

    Entity

---
+++ src/main/java/com/example/Plants/entites/Plant.java
@@ -0,0 +1,64 @@
+package com.example.Plants.entites;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PLANTS")
+public class Plant {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "QUANTITY")
+    private Integer quantity;
+
+    @Column(name = "WATERING_FREQUENCY")
+    private Integer wateringFrequency;
+
+    @Column(name = "HAS_FRUIT")
+    private Boolean hasFruit;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getWateringFrequency() {
+        return wateringFrequency;
+    }
+
+    public void setWateringFrequency(Integer wateringFrequency) {
+        this.wateringFrequency = wateringFrequency;
+    }
+
+    public Boolean getHasFruit() {
+        return hasFruit;
+    }
+
+    public void setHasFruit(Boolean hasFruit) {
+        this.hasFruit = hasFruit;
+    }
+}
--- src/main/java/com/example/Plants/entites/Plants.java
+++
@@ -1,4 +0,0 @@
-package com.example.Plants.entites;
-
-public class Plants {
-}
