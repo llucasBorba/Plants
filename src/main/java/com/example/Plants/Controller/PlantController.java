c39811126aac0d5e37fc42922a919969d958f21c
llucasBorba <lucasborba6075@gmail.com>
2024-02-10 20:18:27 -0300

    Plant Repository

--- src/main/java/com/example/Plants/Controller/PlantController.java
+++ src/main/java/com/example/Plants/Controller/PlantController.java
@@ -1,10 +1,16 @@
 package com.example.Plants.Controller;
 
+import com.example.Plants.Repository.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PlantsController {
+public class PlantController {
+
+    @Autowired
+    private PlantRepository plantRepository;
+
 
     @GetMapping("/Test")
     public String Test(){
