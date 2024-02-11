a42da654fa82679150ab61f33cf1548e03849ead
llucasBorba <lucasborba6075@gmail.com>
2024-02-11 14:14:55 -0300

    New search method

--- src/main/java/com/example/Plants/Controller/PlantController.java
+++ src/main/java/com/example/Plants/Controller/PlantController.java
@@ -4,6 +4,8 @@ import com.example.Plants.Repository.PlantRepository;
 import com.example.Plants.entites.Plant;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -44,9 +46,8 @@ public class PlantController {
         if(p.getQuantity() != null){plantToUpdate.setQuantity(p.getQuantity());}
         if(p.getWateringFrequency() != null){plantToUpdate.setWateringFrequency(p.getWateringFrequency());}
 
-        Plant updatedPlant = plantRepository.save(plantToUpdate);
 
-        return updatedPlant;
+        return plantRepository.save(plantToUpdate);
 
     }
     @DeleteMapping("/plants/{id}")
@@ -57,5 +58,25 @@ public class PlantController {
         Plant plantToDelete = optionalPlant.get();
         plantRepository.delete(plantToDelete);
         return plantToDelete;
+    }
+
+    @GetMapping("/plants/search")
+    public List<Plant> searchPlants(
+            @RequestParam(name = "hasFruit", required=false) Boolean hasFruit,
+            @RequestParam(name = "maxQuantity", required=false) Integer quantity
+    ){
+        if(hasFruit != null && quantity != null){
+            if (hasFruit) {
+                return plantRepository.findByHasFruitTrueAndQuantityLessThan(quantity);
+            }else return plantRepository.findByHasFruitFalseAndQuantityLessThan( quantity);
+
+        }else if (hasFruit != null && hasFruit) {
+            return plantRepository.findByHasFruitTrue();
+        } else if (hasFruit !=null && !hasFruit) {
+            return plantRepository.findByHasFruitFalse();
+        } else if (quantity != null){
+            return plantRepository.findByQuantityLessThan(quantity);
+        }
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/example/Plants/Repository/PlantRepository.java
+++ src/main/java/com/example/Plants/Repository/PlantRepository.java
@@ -3,5 +3,13 @@ package com.example.Plants.Repository;
 import com.example.Plants.entites.Plant;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PlantRepository extends CrudRepository<Plant,Integer> {
+
+    List<Plant> findByHasFruitTrue();
+    List<Plant> findByHasFruitFalse();
+    List<Plant> findByQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+    List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
 }
