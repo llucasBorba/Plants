61ee60b3c39a23460a92b871df54b7099f782646
llucasBorba <lucasborba6075@gmail.com>
2024-02-11 10:14:55 -0300

    CRUD methods

--- src/main/java/com/example/Plants/Controller/PlantController.java
+++ src/main/java/com/example/Plants/Controller/PlantController.java
@@ -31,4 +31,31 @@ public class PlantController {
         newPlant = plantRepository.save(plant);
         return newPlant;
     }
+
+    @PutMapping("/plants/{id}")
+    public Plant updatePlant(@PathVariable("id") Integer id, @RequestBody Plant p){
+        Optional<Plant> optionalPlant = plantRepository.findById(id);
+        if(optionalPlant.isEmpty()){return null;}
+
+        Plant plantToUpdate = optionalPlant.get();
+
+        if(p.getName() != null){plantToUpdate.setName(p.getName());}
+        if(p.getHasFruit() != null){plantToUpdate.setHasFruit(p.getHasFruit());}
+        if(p.getQuantity() != null){plantToUpdate.setQuantity(p.getQuantity());}
+        if(p.getWateringFrequency() != null){plantToUpdate.setWateringFrequency(p.getWateringFrequency());}
+
+        Plant updatedPlant = plantRepository.save(plantToUpdate);
+
+        return updatedPlant;
+
+    }
+    @DeleteMapping("/plants/{id}")
+    public Plant deletePLant(@PathVariable("id") Integer id){
+        Optional<Plant> optionalPlant = plantRepository.findById(id);
+        if(optionalPlant.isEmpty()){return null;}
+
+        Plant plantToDelete = optionalPlant.get();
+        plantRepository.delete(plantToDelete);
+        return plantToDelete;
+    }
 }
